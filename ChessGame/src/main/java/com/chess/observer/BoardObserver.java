3f842c56ec2bb1e137eb169b69b6483a7837ecad
mkmokka <mkmokka12@gmail.com>
2026-06-10 12:18:20 +0600

    Update BoardObserver.java

--- ChessGame/src/main/java/com/chess/observer/BoardObserver.java
+++ ChessGame/src/main/java/com/chess/observer/BoardObserver.java
@@ -1,5 +1,12 @@
 package com.chess.observer;
 
+public class BoardObserver implements Observer {
+    @Override
+    public void update(String message) {
+        System.out.println("Update: " + message);
+    }
+}package com.chess.observer;
+
 public interface Observer {
     void update(String message);
 }
