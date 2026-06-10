6395c8848ac00269a4d0469aba039591901c0e11
mkmokka <mkmokka12@gmail.com>
2026-06-10 12:20:44 +0600

    Update Main.java

--- ChessGame/src/main/java/com/chess/Main.java
+++ ChessGame/src/main/java/com/chess/Main.java
@@ -1,29 +1,27 @@
 package com.chess;
 
+import com.chess.board.Board;
+import com.chess.singleton.GameManager;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
-import com.chess.board.Board;
-import com.chess.singleton.GameManager;
 
 public class Main extends Application {
-
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage stage){
         GameManager gm = GameManager.getInstance();
-
         GridPane root = new GridPane();
-        Board board = new Board();
+        Board board = gm.getBoard();
         root.getChildren().add(board.getGrid());
 
         Scene scene = new Scene(root, 800, 800);
-        primaryStage.setTitle("Chess Game");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        stage.setTitle("Chess Game");
+        stage.setScene(scene);
+        stage.show();
     }
 
-    public static void main(String[] args) {
-        launch(args);
+    public static void main(String[] args){
+        launch();
     }
 }
